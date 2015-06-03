a7a6d0c66c521fb7c5a60ae6693e06f41a325a49
Chris Ortman <chris-ortman@uiowa.edu>
2015-06-02 20:39:13 -0500

    Lot's of updates from some real usage
    * newer firefox, which I load from URL so I don't have
      to keep in git
    * remove some files that probably won't be useful outside
      my specific context

--- cucumber.sh
+++ cucumber.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -t uiris3-box
+LAST_COMMIT=$(GIT_DIR=webapp/source/.git git log -1)
+
+docker run -t webapp-box /bin/bash -c "cd /rails && time(./ci.sh)"
+
+echo "Finished testing GIT COMMIT"
+echo $LAST_COMMIT
